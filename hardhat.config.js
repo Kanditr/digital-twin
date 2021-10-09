9a7ccd8808ec652fdcc000d562e2960d97518828
Kanditr <gun.rodwong@gmail.com>
2021-10-09 11:43:13 +0700

    Sync

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+
+const projectId = "d9ccf5df419749a3a28681741a811e89"
+
+module.exports = {
+  network: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
