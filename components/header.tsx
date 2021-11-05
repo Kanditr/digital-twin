0c381bfca0eb9c83004ff23e5c4e95c38afdcac5
Kanditr <gun.rodwong@gmail.com>
2021-11-05 07:42:28 +0700

    Connect wallet

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 import Link from "next/link";
 import Menu from "./menu";
+import { injected } from "./wallet/connector";
+import { useWeb3React } from "@web3-react/core";
 
 const Header = () => {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
   return (
     //   header
     <header className="border-none relative z-20 pt-12 pb-6 md:py-5 md:border-solid md:border-b-2 border-gray-100">
@@ -23,11 +44,22 @@ const Header = () => {
             Upload
           </button>
         </Link>
-        <Link href="/">
-          <button className="text-center justify-center rounded-full text-sm px-6 h-10 border-2 border-gray-200 hover:bg-blue-500 hover:text-white hover:border-blue-500 transition-all hidden md:block whitespace-nowrap">
-            {"Connect wallet"}
-          </button>
-        </Link>
+        <button
+          onClick={connect}
+          className="text-center justify-center rounded-full text-sm px-6 h-10 border-2 border-gray-200 hover:bg-blue-500 hover:text-white hover:border-blue-500 transition-all hidden md:block whitespace-nowrap mr-3"
+        >
+          {active ? <span>{account}</span> : <span>Connect wallet</span>}
+        </button>
+        <button
+          onClick={disconnect}
+          className={
+            active
+              ? "text-center justify-center rounded-full text-sm px-6 h-10 border-2 border-gray-200 hover:bg-blue-500 hover:text-white hover:border-blue-500 transition-all hidden md:block whitespace-nowrap"
+              : "text-center justify-center rounded-full text-sm px-6 h-10 border-2 border-gray-200 hover:bg-blue-500 hover:text-white hover:border-blue-500 transition-all hidden whitespace-nowrap"
+          }
+        >
+          Disconnect
+        </button>
       </div>
     </header>
   );
